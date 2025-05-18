85d8c361a734171d14a9e1d793e94d36f313daa8
yangjiae12 <yangjiae12@gmail.com>
2025-05-18 11:18:29 +0900

    feat: 가게 등록 컨트롤러 구현 #8

---
+++ src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -0,0 +1,28 @@
+package umc.spring.web.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.converter.StoreConverter;
+import umc.spring.domain.Store;
+import umc.spring.service.store.StoreCommandService;
+import umc.spring.web.dto.StoreRequestDTO;
+import umc.spring.web.dto.StoreResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stores")
+public class StoreRestController {
+
+    private final StoreCommandService storeCommandService;
+
+    @PostMapping
+    public ApiResponse<StoreResponseDTO.CreateResultDTO> create(@RequestBody @Valid StoreRequestDTO.CreateStore request) {
+        Store store = storeCommandService.createStore(request);
+        return ApiResponse.onSuccess(StoreConverter.toCreateResultDTO(store));
+    }
+}
