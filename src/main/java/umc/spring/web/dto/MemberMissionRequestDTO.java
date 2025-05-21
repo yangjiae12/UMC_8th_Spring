084d49237ac3857029c1f1b3f699b4f04f423779
yangjiae12 <yangjiae12@gmail.com>
2025-05-21 11:47:42 +0900

    feat: 미션 중복 도전 방지용 유효성 검증 어노테이션 추가 #11

--- src/main/java/umc/spring/web/dto/MemberMissionRequestDTO.java
+++ src/main/java/umc/spring/web/dto/MemberMissionRequestDTO.java
@@ -2,11 +2,13 @@ package umc.spring.web.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import umc.spring.validation.annotation.NoDuplicateChallenge;
 
 @Getter
 public class MemberMissionRequestDTO {
 
     @Getter
+    @NoDuplicateChallenge
     public static class CreateMemberMission {
         @NotNull
         Long memberId;
