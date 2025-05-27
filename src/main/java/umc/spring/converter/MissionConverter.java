8c2726749966cd5c1d99187a671e28336430dbbb
yangjiae12 <yangjiae12@gmail.com>
2025-05-27 18:51:40 +0900

    feat: 진행 중인 미션 목록 응답 DTO 및 Converter 구현 #20

--- src/main/java/umc/spring/converter/MissionConverter.java
+++ src/main/java/umc/spring/converter/MissionConverter.java
@@ -3,6 +3,7 @@ package umc.spring.converter;
 import org.springframework.data.domain.Page;
 import umc.spring.domain.Mission;
 import umc.spring.domain.Store;
+import umc.spring.domain.mapping.MemberMission;
 import umc.spring.web.dto.MissionRequestDTO;
 import umc.spring.web.dto.MissionResponseDTO;
 
@@ -50,4 +51,29 @@ public class MissionConverter {
                 .isLast(missions.isLast())
                 .build();
     }
+
+    public static MissionResponseDTO.InProgressMissionDTO toInProgressMissionDTO(MemberMission mm) {
+        return MissionResponseDTO.InProgressMissionDTO.builder()
+                .missionSpec(mm.getMission().getMissionSpec())
+                .deadline(mm.getMission().getDeadline())
+                .reward(mm.getMission().getReward())
+                .storeName(mm.getMission().getStore().getName())
+                .build();
+    }
+
+    public static MissionResponseDTO.InProgressMissionListDTO toInProgressMissionListDTO(Page<MemberMission> memberMissions) {
+        List<MissionResponseDTO.InProgressMissionDTO> list = memberMissions.stream()
+                .map(MissionConverter::toInProgressMissionDTO)
+                .collect(Collectors.toList());
+
+        return MissionResponseDTO.InProgressMissionListDTO.builder()
+                .missionList(list)
+                .listSize(list.size())
+                .totalPage(memberMissions.getTotalPages())
+                .totalElements((int) memberMissions.getTotalElements())
+                .isFirst(memberMissions.isFirst())
+                .isLast(memberMissions.isLast())
+                .build();
+    }
+
 }
