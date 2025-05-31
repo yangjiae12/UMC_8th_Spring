7193a83ee2d185ba68db7d53f1cf4fbd184fb61c
yangjiae12 <yangjiae12@gmail.com>
2025-06-01 00:43:52 +0900

    feat: 회원가입에 보안 추가하기 #24

--- src/main/java/umc/spring/web/dto/MemberRequestDTO.java
+++ src/main/java/umc/spring/web/dto/MemberRequestDTO.java
@@ -1,9 +1,12 @@
 package umc.spring.web.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
+import umc.spring.domain.enums.Role;
 import umc.spring.validation.annotation.ExistCategories;
 
 import java.util.List;
@@ -11,6 +14,7 @@ import java.util.List;
 public class MemberRequestDTO {
 
     @Getter
+    @Setter
     public static class JoinDto{
         @NotBlank
         String name;
@@ -28,5 +32,11 @@ public class MemberRequestDTO {
         String specAddress;
         @ExistCategories
         List<Long> preferCategory;
+        @Email
+        String email;    // 이메일 필드 추가
+        @NotBlank
+        String password;    // 비밀번호 필드 추가
+        @NotNull
+        Role role;    // 역할 필드 추가
     }
 }
